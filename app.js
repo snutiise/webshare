4a98430892d850fa48a1fff5ef37b5b22aed65a7
snutiise <snutiise@hanmail.net>
2017-12-06 15:10:10 +0900

    web share project

---
+++ app.js
@@ -0,0 +1,79 @@
+var Client = require('mongodb').MongoClient;
+
+var fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var multer = require('multer'); 
+var upload = multer({ dest: 'uploads/' });
+var crypto = require('crypto');
+
+app.use(express.static(__dirname + '/public'));
+app.use('/file', express.static('uploads'));
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.get('/', function(req,res){
+    res.redirect('http://sodeok.xyz');
+});
+
+function getUserIP(req){
+    var ipAddress;
+
+    if(!!req.hasOwnProperty('sessionID')){
+        ipAddress = req.headers['x-forwarded-for'];
+    } else{
+        if(!ipAddress){
+            var forwardedIpsStr = req.header('x-forwarded-for');
+
+            if(forwardedIpsStr){
+                var forwardedIps = forwardedIpsStr.split(',');
+                ipAddress = forwardedIps[0];
+            }
+            if(!ipAddress){
+                ipAddress = req.connection.remoteAddress;
+            }
+        }
+    }
+    return ipAddress;
+}
+
+app.post('/upload', upload.single('file'), function(req, res){
+    var json=JSON.parse(JSON.stringify(req.file));
+    json.ip=getUserIP(req);
+    json._id=crypto.createHash('md5').update(String(Date.now())).digest("hex");
+    Client.connect('mongodb://localhost:27017/webshare', function(error, db) {
+        if(error) console.log(error);
+        else {
+            db.collection('file').insert(JSON.parse(json),function(doc, err){
+                if(err) console.log(err);
+                res.send(String(json._id));
+            });
+            db.close();
+        }
+    });
+});
+  
+app.post('/download', function(req, res){
+    var key=String(req.body.code);
+    Client.connect('mongodb://localhost:27017/webshare', function(error, db) {
+        if(error) console.log(error);
+        else {
+            db.collection('file').findOne({_id:key},function(err, obj){
+                if(err) console.log(err);
+                if(obj){
+                    res.setHeader('Content-disposition', 'attachment; filename=' + obj.originalname);
+                    res.setHeader('Content-type', obj.mimetype);
+                    
+                    var filestream = fs.createReadStream(obj.path);
+                    filestream.pipe(res);
+                }else  res.send(0);
+            });
+            db.close();
+        }
+    });
+});
+
+app.use((req, res, next) => {
+    res.status(404).redirect('http://sodeok.xyz');
+});
+app.listen(8001);
